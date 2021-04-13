fdae4e8a3275bf22cee036a774d981527918460d
Suvash Shah Thakuri <suvash@appmozart.com>
2021-04-12 23:17:57 -0700

    Search Insert Position

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ Leetcode Problems and my attempts to solve them
 
 * [Remove Duplicates from Sorted Array](array/remove-duplicates-from-sorted-array/)
 * [Remove Element](array/remove-element/)
+* [Search Insert Position](array/search-insert-position/)
---
+++ array/search-insert-position/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace search_insert_position
+{
+    //URL: https://leetcode.com/problems/search-insert-position/
+    /*
+    Given a sorted array of distinct integers and a target value, return the index if the target is found. 
+    If not, return the index where it would be if it were inserted in order.
+    */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var i = SearchInsert_Solution2(new int[]{1},2);
+            Console.WriteLine(i);
+        }
+
+        public static int SearchInsert_Solution1(int[] nums, int target) 
+        {
+            int i=0;
+            for(; i< nums.Length; i++)
+            {
+                if(nums[i] >= target)
+                {
+                    return i;
+                }
+            }
+            return i;
+        }
+
+        // Divide into half and perform search on half
+        public static int SearchInsert_Solution2(int[] nums, int target) 
+        {
+            int len = nums.Length/2 + (nums.Length%2==0? 0: 1);
+            int i=0;
+            if(nums.Length>1 && nums[len]<target)
+            {
+                i= len-1;
+                len= nums.Length;
+            }
+            for(; i< len; i++)
+            {
+                if(nums[i] >= target)
+                {
+                    return i;
+                }
+            }
+            return i;
+        }
+    }
+}
