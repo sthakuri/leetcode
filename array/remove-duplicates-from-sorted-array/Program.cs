8a62b5c467a1868e74d558cfb5be634409869d78
Suvash Shah Thakuri <suvash@appmozart.com>
2021-04-12 21:20:02 -0700

    Remove Duplicates from Sorted Array

---
+++ array/remove-duplicates-from-sorted-array/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace remove_duplicates_from_sorted_array
+{
+    //URL: https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+
+    /*
+    
+        Given a sorted array nums, remove the duplicates in-place such that 
+        each element appears only once and returns the new length.
+
+        Do not allocate extra space for another array, you must do this by modifying 
+        the input array in-place with O(1) extra memory.    
+    */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var len = RemoveDuplicates(new int[] {});
+            Console.WriteLine(len);
+        }
+        public static int RemoveDuplicates(int[] nums) 
+        {
+            int n = nums.Length>0? 1: 0;
+            for(int i=1; i< nums.Length; i++)
+            {
+                if(nums[i] != nums[n-1])
+                {
+                    nums[n]= nums[i];
+                    n++;
+                }
+            }
+            return n;
+        }
+    }
+}
